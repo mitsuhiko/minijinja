bf8c099febf4314a89361351b00c0c5134aeb840
Armin Ronacher <armin.ronacher@active-4.com>
2021-09-24 18:56:20 +0200

    Improve expr example

--- examples/expr.rs
+++ examples/expr.rs
@@ -1,14 +1,11 @@
 use minijinja::Environment;
-use serde::Serialize;
-
-#[derive(Serialize)]
-pub struct Context {
-    foo: usize,
-}
+use std::env;
 
 fn main() {
+    let args = env::args().collect::<Vec<_>>();
     let env = Environment::new();
-    let expr = env.compile_expression("foo == 42").unwrap();
-    let result = expr.eval(&Context { foo: 42 }).unwrap();
-    println!("result: {:?}", result);
+    let expr = env.compile_expression(&args[1]).unwrap();
+    let result = expr.eval(&()).unwrap();
+    let serialized = serde_json::to_string_pretty(&result).unwrap();
+    println!("{}", serialized);
 }
