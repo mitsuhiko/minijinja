971278e7d26c2599f852bc720a3898bfffc0da9c
clemkeirua <clement@keiruaprod.fr>
2021-09-24 10:59:00 +0200

    added inheritance example

---
+++ examples/inheritance.rs
@@ -0,0 +1,43 @@
+use minijinja::Environment;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Page {
+    title: String,
+    content: String,
+}
+
+#[derive(Serialize)]
+pub struct Context {
+    page: Page,
+}
+
+fn main() {
+    let mut env = Environment::new();
+    env.add_template(
+        "base.html",
+        r#"<html>
+    <head><title>{% block title %}some website{% endblock %}</title></head>
+    <body>{% block body %}{% endblock %}</body></html>"#,
+    )
+    .unwrap();
+    env.add_template(
+        "index.html",
+        r#"{% extends "base.html" %}
+{% block title %}{{ page.title | upper }} | {{ super() }}{% endblock %}
+{% block body %}{{ page.content }}{% endblock %}"#,
+    )
+    .unwrap();
+    let template = env.get_template("index.html").unwrap();
+    println!(
+        "{}",
+        template
+            .render(&Context {
+                page: Page {
+                    title: "Some title".into(),
+                    content: "Lorum Ipsum".into(),
+                },
+            })
+            .unwrap()
+    );
+}
