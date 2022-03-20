5a4fad87542f4f946c211db4036fd0d91a3472ae
Armin Ronacher <armin.ronacher@active-4.com>
2022-03-20 21:58:57 +0100

    Added dynamic loading example

---
+++ examples/loader/README.md
@@ -0,0 +1,12 @@
+# loader
+
+This example demonstrates how templates can be loaded at runtime from
+the file system (in this case from the `templates` folder).
+
+```console
+$ cargo run
+header
+Hello World!
+footer
+```
+
---
+++ examples/loader/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+
+use minijinja::{context, Environment, Error, ErrorKind, Source};
+
+fn main() {
+    let mut env = Environment::new();
+    let template_path = std::env::current_dir().unwrap().join("templates");
+
+    env.set_source(Source::with_loader(move |name| {
+        let pieces = name.split('/');
+        let mut path = template_path.clone();
+        for piece in pieces {
+            if piece != "." && piece != ".." && !piece.contains('\\') {
+                path.push(piece);
+            } else {
+                return Ok(None);
+            }
+        }
+
+        match fs::read_to_string(path) {
+            Ok(result) => Ok(Some(result)),
+            Err(err) => {
+                if err.kind() == std::io::ErrorKind::NotFound {
+                    Ok(None)
+                } else {
+                    Err(
+                        Error::new(ErrorKind::TemplateNotFound, "failed to load template")
+                            .with_source(err),
+                    )
+                }
+            }
+        }
+    }));
+
+    let tmpl = env.get_template("hello.txt").unwrap();
+    println!(
+        "{}",
+        tmpl.render(context! {
+            name => "World"
+        })
+        .unwrap()
+    );
+}
