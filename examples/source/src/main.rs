22a85260055bd0a1a88ee365b231f8bd46bbc299
Armin Ronacher <armin.ronacher@active-4.com>
2022-08-22 00:04:41 +0200

    once_cell over lazy_static

--- examples/source/src/main.rs
+++ examples/source/src/main.rs
@@ -1,18 +1,14 @@
-use lazy_static::lazy_static;
 use minijinja::{Environment, Source};
+use once_cell::sync::Lazy;
 use std::collections::BTreeMap;
 
-lazy_static! {
-    static ref ENV: Environment<'static> = create_env();
-}
-
-fn create_env() -> Environment<'static> {
+static ENV: Lazy<Environment<'static>> = Lazy::new(|| {
     let mut env = Environment::new();
     let mut source = Source::new();
     source.load_from_path("templates", &["txt"]).unwrap();
     env.set_source(source);
     env
-}
+});
 
 fn main() {
     let mut ctx = BTreeMap::new();
