2499ce605647228607ea2ba97dd50d68bc123928
Armin Ronacher <armin.ronacher@active-4.com>
2022-09-25 10:56:49 +0200

    Rustfmt

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -2,8 +2,7 @@ use minijinja::{context, Environment};
 
 fn main() {
     let mut env = Environment::new();
-    env.add_template("hello.txt", "Hello {{ name }}!")
-        .unwrap();
+    env.add_template("hello.txt", "Hello {{ name }}!").unwrap();
     let template = env.get_template("hello.txt").unwrap();
     println!("{}", template.render(context!(name => "John")).unwrap());
 }
