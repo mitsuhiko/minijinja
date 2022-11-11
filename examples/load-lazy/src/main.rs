0bea909a796dd731aaf06b9486ae40e242b979c9
Armin Ronacher <armin.ronacher@active-4.com>
2022-11-11 11:08:18 +0100

    Added lazy loading example

---
+++ examples/load-lazy/src/main.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs;
+
+use minijinja::value::Value;
+use minijinja::{Environment, Error, ErrorKind};
+
+fn load_data(filename: &str) -> Result<Value, Error> {
+    let mut rv = env::current_dir().unwrap().join("src");
+    for segment in filename.split('/') {
+        if segment.starts_with('.') || segment.contains('\\') {
+            return Err(Error::new(ErrorKind::InvalidOperation, "bad filename"));
+        }
+        rv.push(segment);
+    }
+    let contents = fs::read(&rv).map_err(|err| {
+        Error::new(ErrorKind::InvalidOperation, "could not read JSON file").with_source(err)
+    })?;
+    let parsed: serde_json::Value = serde_json::from_slice(&contents[..])
+        .map_err(|err| Error::new(ErrorKind::InvalidOperation, "invalid JSON").with_source(err))?;
+    Ok(Value::from_serializable(&parsed))
+}
+
+fn main() {
+    let mut env = Environment::new();
+    env.add_function("load_data", load_data);
+    env.add_template("template.html", include_str!("template.html"))
+        .unwrap();
+
+    let tmpl = env.get_template("template.html").unwrap();
+    println!("{}", tmpl.render(()).unwrap());
+}
