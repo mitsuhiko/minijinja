bcc7f44c1fe5efb9274b0d9a9d24b773baf61627
Michael Michel <michael.spiegelhalter@gmx.de>
2022-11-20 14:38:53 +0100

    edited docstring

--- minijinja/src/filters.rs
+++ minijinja/src/filters.rs
@@ -756,16 +756,17 @@ mod builtins {
     ///
     /// This filter is only available if the `indent` feature is enabled.
     /// The first optional parameter to the filter can be set to `true` to
-    /// indent the first line
+    /// indent the first line. The parameter defaults to false.
     /// the second optional parameter to the filter can be set to `true`
-    /// to indent blank lines
+    /// to indent blank lines. The parameter defaults to false.
     /// This filter is useful, if you want to template yaml-files
     ///
     /// ```jinja
     /// example:
     ///   config:
+    /// {{ global_conifg|indent(2) }} #does not indent first line
     /// {{ global_config|indent(2,true) }} #indent whole Value with two spaces
-    /// {{ global_conifg|indent(2,false) }} #indent
+    /// {{ global_config|indent(2,true,true)}} #indents whole multiline value
     /// ```
     #[cfg_attr(docsrs, doc(cfg(all(feature = "builtins", feature = "indent"))))]
     #[cfg(feature = "indent")]
