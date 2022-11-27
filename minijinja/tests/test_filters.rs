f4675a1ed2508b3900063619dc731101bfcb82e3
Armin Ronacher <armin.ronacher@active-4.com>
2022-11-27 20:08:49 +0100

    Isolate filter tests

---
+++ minijinja/tests/test_filters.rs
@@ -0,0 +1,50 @@
+#![cfg(feature = "builtins")]
+use minijinja::filters::indent;
+
+#[test]
+fn test_indent_one_empty_line() {
+    let teststring = String::from("\n");
+    assert_eq!(indent(teststring, 2, None, None), String::from(""));
+}
+
+#[test]
+fn test_indent_one_line() {
+    let teststring = String::from("test\n");
+    assert_eq!(indent(teststring, 2, None, None), String::from("test"));
+}
+
+#[test]
+fn test_indent() {
+    let teststring = String::from("test\ntest1\n\ntest2\n");
+    assert_eq!(
+        indent(teststring, 2, None, None),
+        String::from("test\n  test1\n\n  test2")
+    );
+}
+
+#[test]
+fn test_indent_with_indented_first_line() {
+    let teststring = String::from("test\ntest1\n\ntest2\n");
+    assert_eq!(
+        indent(teststring, 2, Some(true), None),
+        String::from("  test\n  test1\n\n  test2")
+    );
+}
+
+#[test]
+fn test_indent_with_indented_blank_line() {
+    let teststring = String::from("test\ntest1\n\ntest2\n");
+    assert_eq!(
+        indent(teststring, 2, None, Some(true)),
+        String::from("test\n  test1\n  \n  test2")
+    );
+}
+
+#[test]
+fn test_indent_with_all_indented() {
+    let teststring = String::from("test\ntest1\n\ntest2\n");
+    assert_eq!(
+        indent(teststring, 2, Some(true), Some(true)),
+        String::from("  test\n  test1\n  \n  test2")
+    );
+}
