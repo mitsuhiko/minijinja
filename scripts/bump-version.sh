e4a18f46b56d505e20cb213b13e65fd64a5003c2
Armin Ronacher <armin.ronacher@active-4.com>
2023-01-18 16:36:42 +0100

    Added meta information for python package

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -8,6 +8,7 @@ NEW_VERSION="${1}"
 
 echo "Bumping version: ${NEW_VERSION}"
 perl -pi -e "s/\bminijinja v.*? /minijinja v$NEW_VERSION /" README.md
+perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" minijinja-py/pyproject.toml
 perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" minijinja/Cargo.toml
 perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" minijinja-*/Cargo.toml
 perl -pi -e "s/^(minijinja.*?)version = \".*?\"/\$1version = \"$NEW_VERSION\"/" minijinja-*/Cargo.toml
