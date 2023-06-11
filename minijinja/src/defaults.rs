14e90c26337139495b117bd8c752ef73b3f4d717
Armin Ronacher <armin.ronacher@active-4.com>
2023-06-11 18:40:59 +0200

    Added unique filter

--- minijinja/src/defaults.rs
+++ minijinja/src/defaults.rs
@@ -89,6 +89,8 @@ pub(crate) fn get_builtin_filters() -> BTreeMap<Cow<'static, str>, filters::Boxe
         rv.insert("selectattr".into(), BoxedFilter::new(filters::selectattr));
         rv.insert("rejectattr".into(), BoxedFilter::new(filters::rejectattr));
         rv.insert("map".into(), BoxedFilter::new(filters::map));
+        rv.insert("unique".into(), BoxedFilter::new(filters::unique));
+        rv.insert("pprint".into(), BoxedFilter::new(filters::pprint));
 
         #[cfg(feature = "json")]
         {
