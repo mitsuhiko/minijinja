f97fd2a00a814b8546bb68ff3b0a7008a9ba9d58
Armin Ronacher <armin.ronacher@active-4.com>
2023-07-02 22:21:26 +0200

    Make code slightly more readable in example

--- examples/call-block-function/src/main.rs
+++ examples/call-block-function/src/main.rs
@@ -3,32 +3,28 @@ use std::iter::FromIterator;
 use minijinja::value::{Kwargs, Value};
 use minijinja::{Environment, Error, ErrorKind, State};
 
+fn custom_loop(state: &State, num: i64, kwargs: Kwargs) -> Result<String, Error> {
+    let mut rv = String::new();
+    let caller = kwargs.get::<Value>("caller")?;
+    kwargs.assert_all_used()?;
+    for it in 0..num {
+        let rendered = caller.call(
+            state,
+            &[Kwargs::from_iter([("it", Value::from(it + 1))]).into()],
+        )?;
+        rv.push_str(rendered.as_str().ok_or_else(|| {
+            Error::new(
+                ErrorKind::InvalidOperation,
+                "caller did not return a string",
+            )
+        })?);
+    }
+    Ok(rv)
+}
+
 fn main() {
     let mut env = Environment::new();
-    env.add_function(
-        "custom_loop",
-        |state: &State, num: i64, kwargs: Kwargs| -> Result<String, Error> {
-            let caller = kwargs.get::<Value>("caller")?;
-            kwargs.assert_all_used()?;
-            let mut rv = String::new();
-            for it in 0..num {
-                let kwargs = Kwargs::from_iter([("it", Value::from(it + 1))]);
-                rv.push_str(
-                    caller
-                        .call(state, &[kwargs.into()])?
-                        .as_str()
-                        .ok_or_else(|| {
-                            Error::new(
-                                ErrorKind::InvalidOperation,
-                                "caller did not return a string",
-                            )
-                        })?,
-                );
-            }
-            Ok(rv)
-        },
-    );
-
+    env.add_function("custom_loop", custom_loop);
     let tmpl = env.template_from_str(include_str!("demo.txt")).unwrap();
     println!("{}", tmpl.render(()).unwrap());
 }
