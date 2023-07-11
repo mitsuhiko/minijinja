f2625ebc8bf87c196a0baf8d3d464de1da7c0b3c
Armin Ronacher <armin.ronacher@active-4.com>
2023-07-12 01:01:18 +0200

    Change yaml example to github actions style syntax

--- examples/generate-yaml/src/main.rs
+++ examples/generate-yaml/src/main.rs
@@ -2,10 +2,19 @@
 use std::collections::BTreeMap;
 use std::env;
 
-use minijinja::{context, Environment};
+use minijinja::{context, Environment, Syntax};
 
 fn main() {
     let mut env = Environment::new();
+    env.set_syntax(Syntax {
+        block_start: "{%".into(),
+        block_end: "%}".into(),
+        variable_start: "${{".into(),
+        variable_end: "}}".into(),
+        comment_start: "{#".into(),
+        comment_end: "#}".into(),
+    })
+    .unwrap();
     env.add_template("template.yml", include_str!("template.yaml"))
         .unwrap();
     let tmpl = env.get_template("template.yml").unwrap();
