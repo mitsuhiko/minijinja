0e3c338baa6f2b235f1db7336d016d8d1d4b3fe9
Armin Ronacher <armin.ronacher@active-4.com>
2023-07-18 10:04:53 +0200

    Added clarifying comment

--- examples/merge-context/src/main.rs
+++ examples/merge-context/src/main.rs
@@ -5,6 +5,8 @@ use minijinja_contrib::add_to_environment;
 fn render_template(env: &Environment, tmpl: &str, ctx: Value) -> String {
     let tmpl = env.get_template(tmpl).unwrap();
     tmpl.render(context! {
+        // This variable is used by `datetimeformat` to pick the right
+        // datetime format.
         DATETIME_FORMAT => "full",
         ..ctx
     })
