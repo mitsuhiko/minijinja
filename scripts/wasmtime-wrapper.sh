4d4881f3d053c731d69655a76526e91ae6b2589e
Armin Ronacher <armin.ronacher@active-4.com>
2023-09-29 12:04:36 +0200

    Raise wasm stack in wasmtime

--- scripts/wasmtime-wrapper.sh
+++ scripts/wasmtime-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
-wasmtime run --env INSTA_WORKSPACE_ROOT=/ --mapdir "/::$(pwd)" -- "$@"
+wasmtime run -Wmax-wasm-stack=1048576 --env INSTA_WORKSPACE_ROOT=/ --mapdir "/::$(pwd)" -- "$@"
