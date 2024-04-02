52cb8f967f30edd5c67c839710f39503dcb43393
Armin Ronacher <armin.ronacher@active-4.com>
2024-04-02 15:21:52 +0200

    Restore WASI Support (#468)

--- scripts/wasmtime-wrapper.sh
+++ scripts/wasmtime-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
-wasmtime run --max-wasm-stack=1048576 --env INSTA_WORKSPACE_ROOT=/ --mapdir "/::$(pwd)" -- "$@"
+wasmtime run --max-wasm-stack=4194304 --env INSTA_WORKSPACE_ROOT=/ --mapdir "/::$(pwd)" -- "$@"
