2d7b68b81c7236e4c9245d4682dc64f15d8fefac
Armin Ronacher <armin.ronacher@active-4.com>
2024-09-16 19:28:56 +0200

    Add an Value::is_integer method

--- minijinja/src/value/mod.rs
+++ minijinja/src/value/mod.rs
@@ -979,6 +979,14 @@ impl Value {
         )
     }
 
+    /// Returns true if the number is a real integer.
+    pub fn is_integer(&self) -> bool {
+        matches!(
+            self.0,
+            ValueRepr::U64(_) | ValueRepr::I64(_) | ValueRepr::I128(_) | ValueRepr::U128(_)
+        )
+    }
+
     /// Returns `true` if the map represents keyword arguments.
     pub fn is_kwargs(&self) -> bool {
         Kwargs::extract(self).is_some()
