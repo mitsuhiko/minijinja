31035e238cd3285b6705ad4435ff66a370b10305
Yassir Barchi <yassir@numberly.com>
2024-12-02 09:43:56 +0100

    fix tests

--- minijinja-contrib/tests/filters.rs
+++ minijinja-contrib/tests/filters.rs
@@ -1,5 +1,5 @@
 use minijinja::{context, Environment};
-use minijinja_contrib::filters::{pluralize, wordcount};
+use minijinja_contrib::filters::pluralize;
 use similar_asserts::assert_eq;
 
 #[test]
@@ -204,7 +204,10 @@ fn test_truncate() {
 }
 
 #[test]
+#[cfg(feature = "wordwrap")]
 fn test_wordcount() {
+    use minijinja_contrib::filters::wordcount;
+
     let mut env = Environment::new();
     env.add_filter("wordcount", wordcount);
 
