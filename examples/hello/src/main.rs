abe8f4ff5335af651d7f2cebcdb87e5db2957575
Armin Ronacher <armin.ronacher@active-4.com>
2025-01-30 16:06:12 +0100

    Change to DeserializeOwned

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -2,6 +2,7 @@ use minijinja::{context, Environment};
 
 fn main() {
     let mut env = Environment::new();
+
     env.add_template("hello.txt", "Hello {{ name }}!").unwrap();
     let template = env.get_template("hello.txt").unwrap();
     println!("{}", template.render(context!(name => "John")).unwrap());
